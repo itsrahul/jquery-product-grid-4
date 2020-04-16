1e70abe0cff0c6ccd520d931d4ba50ada8d82977
itsrahul <rahul17a1997@gmail.com>
2020-04-16 13:39:03 +0530

    Add files via upload

--- js/UrlHash.js
+++ js/UrlHash.js
@@ -6,9 +6,16 @@ export default class UrlHash
 
     this.pageSize = Number(this.url.get("pageSize")) || 3;
     this.selectedIndex = this.pageSize/3 - 1;
-    this.brandFilter = this.url.get("brand") ? new Set(this.url.get("brand").split(", ")) : new Set();
-    this.colorFilter = this.url.get("color") ? new Set(this.url.get("color").split(", ")) : new Set();
+    this.brandFilter = this.url.get("brand") ? new Set(this.url.get("brand").split(",")) : new Set();
+    this.colorFilter = this.url.get("color") ? new Set(this.url.get("color").split(",")) : new Set();
     this.showAvailable = this.url.get("showAvailable");
     this.displayPage = this.url.get("displayPage") || 1;
+
+    // this.pageSize = 6;
+    // this.selectedIndex = 1;
+    // this.brandFilter = new Set(["BRAND A", "BRAND B"]);
+    // this.colorFilter = new Set(["Green"]);
+    // this.showAvailable = false;
+    // this.displayPage = 2;
   }
 }
